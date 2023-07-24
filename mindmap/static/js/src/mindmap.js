65df3267bb906755444ee096d46658cd9486eae3
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-07-24 16:49:29 -0500

    feat: set height of jsmind div and add save button without functionality.

--- mindmap/static/js/src/mindmap.js
+++ mindmap/static/js/src/mindmap.js
@@ -1,25 +1,31 @@
 function MindMapXBlock(runtime, element) {
-    console.log("This is the MindMapXBlock");
-    (function () {
-        var mind = {
-            "meta": {
-                "name": "jsMind remote",
-                "author": "eduNEXT",
-                "version": "0.1"
-            },
-            "format": "node_array",
-            "data": [
-                { "id": "root", "isroot": true, "topic": "Root" },
-            ]
-        };
+    var mind = {
+        "meta": {
+            "name": "Mind Map",
+            "author": "eduNEXT",
+            "version": "0.1"
+        },
+        "format": "node_array",
+        "data": [
+            { "id": "root", "isroot": true, "topic": "Root" },
+        ]
+    };
 
-        var options = {
-            container: 'jsmind_container',
-            editable: true,
-            theme: 'asphalt',
-        };
+    var options = {
+        container: 'jsmind_container',
+        editable: true,
+        theme: 'asphalt',
+    };
 
-        var jm = new jsMind(options);
-        jm.show(mind);
-    })();
+    var jm = new jsMind(options);
+    jm.show(mind);
+
+    function saveMindMap() {
+        const jsonData = jm.get_data('node_array');
+        const data = JSON.stringify(jsonData);
+        console.log(data);
+    }
+
+    const saveButton = document.getElementById('save');
+    saveButton.addEventListener('click', saveMindMap);
 }
