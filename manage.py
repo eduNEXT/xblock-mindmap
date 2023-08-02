379d63cd5be5e25b67593e700a861b05f53005bb
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-08-02 11:18:51 -0500

    test: add tests of mind map xblock

--- manage.py
+++ manage.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 """
-A django manage.py file.
-
-It eases running django related commands with the correct settings already
-imported.
+Django administration utility.
 """
+
 import os
 import sys
 
-from django.core.management import execute_from_command_line
+PWD = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == "__main__":
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "mindmap.locale.settings"
-    )
-
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mindmap.settings.test")
+    sys.path.append(PWD)
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django  # pylint: disable=unused-import
+        except ImportError as import_error:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from import_error
+        raise
     execute_from_command_line(sys.argv)
