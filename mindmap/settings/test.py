fe6fec76cc7cfb2acc70457700d6312b8f844a1f
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-09-14 17:19:35 -0500

    feat: add statici18n in installed apps and required configuration

--- mindmap/settings/test.py
+++ mindmap/settings/test.py
@@ -24,6 +24,7 @@ DATABASES = {
 
 
 INSTALLED_APPS = (
+    'statici18n',
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "mindmap",
@@ -40,6 +41,23 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 
 USE_TZ = True
+
+# statici18n
+# https://django-statici18n.readthedocs.io/en/latest/settings.html
+
+LANGUAGES = [
+    ('en', 'English - Source Language'),
+    ('es_419', 'Spanish (Latin America)'),
+    ('es_ES', 'Spanish (Spain)'),
+]
+
+STATICI18N_DOMAIN = 'text'
+STATICI18N_NAMESPACE = 'MindMapI18N'
+STATICI18N_PACKAGES = (
+    'mindmap',
+)
+STATICI18N_ROOT = 'mindmap/public/js'
+STATICI18N_OUTPUT_DIR = 'translations'
 
 # Mind Map plugin settings
 MINDMAP_XMODULE_BACKEND = 'mindmap.edxapp_wrapper.backends.xmodule_p_v1'
